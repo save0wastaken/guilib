adf530e745c0355332a117e7b2a15ac3e611c289
save0wastaken <88059092+save0wastaken@users.noreply.github.com>
2021-11-29 17:58:17 -0600

    Create AUDIO.lua

---
+++ AUDIO.lua
@@ -0,0 +1,68 @@
+local audios = {
+	{
+		ID = 4524572739;
+		Name = "Gunna - Drip Too Hard (30s)"
+	};
+    {
+		ID = 1023116114;
+		Name = "I'm Mr. Meeseeks!"
+	};
+    {
+		ID = 850994651;
+		Name = "You're Welcome (Rick and Morty)"
+	};
+    {
+		ID = 3043669806;
+		Name = "Rick and Morty Intro"
+	};
+    {
+		ID = 6817255053;
+		Name = "Lil Mosey - Noticed"
+	};
+    {
+		ID = 3180460921;
+		Name = "Old Town Road OOFED"
+	};
+    {
+		ID = 3478313680;
+		Name = "Juice WRLD - Lucid Dreams"
+	};
+    {
+		ID = 2590490779;
+		Name = "Crab Rave OOFED"
+	};
+    {
+		ID = 261605308;
+		Name = "Souja Boy - Rick and Morty"
+	};
+    {
+		ID = 4551697370;
+		Name = "Arizona Zervas - Roxanne (Remix)"
+	};
+    {
+		ID = 250408242;
+		Name = "Bossfight - Milky Ways"
+	};
+    {
+		ID = 7199899359;
+		Name = "Lil Nas X + Masked Wolf - Industry Baby In The Ocean (Remix)"
+	};
+	{
+		ID = 4877401833;
+		Name = "Travis Scott - ASTROTHUNDER (Loud)"
+	};
+	{
+		ID = 2624911152;
+		Name = "XXXTENTACION - Look at me!"
+	};
+	{
+		ID = 3131373393;
+		Name = "XXXTENTACION - Moonlight (Nin9 Remix)"
+	};
+	{
+		ID = 7160494327;
+		Name = "XXXTENTACION - Hope (Slowed)"
+	};
+}
+-- i could have used JSON decoding, but I was lazy
+shared.audios = audios
